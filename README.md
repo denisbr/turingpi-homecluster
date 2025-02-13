4f8a195b11828d747cf68376f69a06374f6f748b
Denis Brækhus <denis@denis.no>
2025-02-13 12:58:55 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# TuringPi Home Cluster v0.01
+
+TL;DR:
+
+* Talos Linux 1.9.3
+* TuringPi RK1 nodes x3 (8GB RAM)
+* Talhelper (https://budimanjojo.github.io/talhelper)
+* sops / age / 1password
+
+## What is this
+
+A very basic cluster init to learn as much as possible about Talos and the TuringPi API/CLI.
+Chose to not go directly to IaC for this one to learn more and not abstract away everything right off the bat.
---
+++ setup-talos.sh
@@ -0,0 +1,13 @@
+brew install talhelper 1password-cli age sops talosctl ubi
+ubi -v --in ~/bin/ --project https://github.com/turing-machines/tpi/
+for nodenr in 1 2 3; do tpi flash -n $nodenr -i metal-arm64.raw; done
+talhelper gensecret >talsecret.sops.yaml
+op read op://Private/Age-Key/password >~/Library/Application\ Support/sops/age/keys.txt
+sops --encrypt -i --age "$(cat public_age_keys.txt)" talsecret.sops.yaml
+talhelper genconfig
+cp clusterconfig/talosconfig ~/.talos/config
+talosctl apply-config --insecure -n 192.168.1.52 --file clusterconfig/homecluster-node1.yaml
+talosctl apply-config --insecure -n 192.168.1.44 --file clusterconfig/homecluster-node2.yaml
+talosctl apply-config --insecure -n 192.168.1.108 --file clusterconfig/homecluster-node3.yaml
+talosctl --cluster homecluster -n 192.168.1.52 --talosconfig clusterconfig/talosconfig kubeconfig .
+talosctl patch mc --patch "$(sops --decrypt --age "$(cat public_age_keys.txt)" tailscale-config.sops.yaml)"
